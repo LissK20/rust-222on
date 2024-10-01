549e214489e51bd8754e0a6dfca75e390b1a3b12
Liss Kamellia <k.liss@e-u.edu.ua>
2024-10-01 19:37:04 +0300

    chapter03

--- src/chapter03.rs
+++ src/chapter03.rs
@@ -1,13 +1,25 @@
 #[test]
-fn test() {
+fn test()
+{
     assert_eq!(2 + 3, 5);
 }
 
 #[test]
-fn test1() {
+fn test1()
+{
     let x: i32 = 5; // Uninitialized but used, ERROR !
     let _y: i32; // Uninitialized but also unused, only a Warning !
 
     assert_eq!(x, 5);
+    println!("Success!");
+}
+
+#[test]
+fn test2()
+{
+    let mut x = 1;
+    x += 2;
+
+    assert_eq!(x, 3);
     println!("Success!");
 }
